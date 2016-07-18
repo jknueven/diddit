06c806c752a2f3871834e1265c32e259480984bb
Jared <jcknueven@gmail.com>
2016-07-18 13:17:34 -0400

    added views

---
+++ js/mainView.js
@@ -0,0 +1,53 @@
+(function(){
+'use strict'
+
+angular
+.module('diddit', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise("/#");
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		views: {
+			'main': {
+				templateUrl: '../views/home.html',
+				controller: 'homeController',
+				controllerAs: 'home',
+			},
+			'nav': {
+				templateUrl:'../views/partials/nav.html',
+			},
+		},
+	})
+	.state('comment', {
+		url: '/comment',
+		views: {
+			'main': {
+				templateUrl: '../views/comment.html',
+				controller: 'commentController',
+				controllerAs: 'comment',
+			},
+			'nav': {
+				templateUrl:'../views/partials/nav.html',
+			},
+		},
+	})
+	.state('post', {
+		url: '/post',
+		views: {
+			'main': {
+				templateUrl: '../views/post.html',
+				controller: 'postController',
+				controllerAs: 'post',
+			},
+			'nav': {
+				templateUrl:'../views/partials/nav.html',
+			},
+		},
+	});
+
+});
+	
+}());
