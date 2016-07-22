83a1b9cb27f5e0f443af4a0407809f1b9b580fa4
Jared <jcknueven@gmail.com>
2016-07-22 13:59:36 -0400

    fixed comment submission and display

--- js/controllers/commentController.js
+++ js/controllers/commentController.js
@@ -1,6 +1,6 @@
   'use strict';
 
-angular.module("diddit").controller("commentController", function(API, $stateParams) {
+angular.module("diddit").controller("commentController", function(API, $stateParams,$scope,$state) {
 
 	var vm = this;
 	
@@ -9,20 +9,22 @@ angular.module("diddit").controller("commentController", function(API, $statePar
 	var data = API.getAPost(id);
 
 
-	 data.then(function(post){
+	 	data.then(function(post){
 	 	
-	 	vm.data = post.data;
-
-	 });
-
+	 		vm.data = post.data;
+	 	});
 
 	 vm.submit = function(){
+	 	vm.form.post_id = vm.data.id;
 		var postComment = API.postComment(vm.form);
 		
 		 postComment.then(function(response){
 		 	console.log(response);
 		 	
 		 	vm.form = "";
+
+		 	$state.reload();
+
 
 		 });
 
